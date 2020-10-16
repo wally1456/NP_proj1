35299627181dd23f0ec3f12c9d04378ae409e7ed
wally <wally1456@gmail.com>
2020-10-16 23:23:38 +0800

    wrong number pipe

--- npshell.cpp
+++ npshell.cpp
@@ -31,40 +31,74 @@ void write_to_file(char *filename){
 }
 
 void close_pipe(int pipeline[][2],int pipe_count){
+    if (pipe_count != 0){
     close(pipeline[pipe_count-1][0]);
     close(pipeline[pipe_count-1][1]);
+    }
     close(pipeline[pipe_count][0]);
     close(pipeline[pipe_count][1]);
 }
 
-void excute_cmd(vector<string> s_input){
+void childHandler(int signo){ 
+    int status; 
+    while (waitpid(-1, &status, WNOHANG) > 0) { 
+            //do nothing 
+        } 
+    }
+
+void excute_cmd(vector<string> s_input,vector<vector<int>> &number_pipe,int cmd_count){
+    signal(SIGCHLD, childHandler);
     int i = 0;
     vector<string> tmp_input;
-
     int pipeline[1000][2];//maximum command = 1000
     int pipe_count = -1;
+    vector<pid_t> pidTable;
+    bool is_number_pipe = 0;
+    bool have_number_pipe = 0;
+    int number;
+
+    if (!number_pipe[cmd_count%1000].empty()){
+        have_number_pipe = 1;
+    }
+
     while(i<s_input.size()){
         if (s_input[i]=="|" || s_input[i]==">" || i == s_input.size()-1){
 
-            if(i == s_input.size()-1)
-                tmp_input.push_back(s_input[i]);                       
+            if(i == s_input.size()-1 && s_input[i][0]=='|'){
+                s_input[i].erase(0,1);
+                number = stoi(s_input[i].c_str());
+                is_number_pipe=1;
+            }
+            else if(i == s_input.size()-1)
+                tmp_input.push_back(s_input[i]);      
+
             pipe_count += 1 ;
 
             if(pipe(pipeline[pipe_count])<0)
-                cerr << "create pipe error" << std::endl;         
+                cerr << "create pipe error" << std::endl;
 
-            if(fork()==0){//child
-                cerr << STDIN_FILENO << "\t" << STDOUT_FILENO << endl;
-
-                cerr << pipeline[pipe_count][0] << "\t" << pipeline[pipe_count][1] << endl<< endl;
+            pid_t pid;
+            while((pid = fork())<0){
+                usleep(1000);
+            }
+            pidTable.push_back(pid);
+            if(pid==0){//child
                 if (pipe_count != 0)
                     dup2(pipeline[pipe_count-1][0],STDIN_FILENO); 
-                if (i != s_input.size()-1)
+                else if (have_number_pipe){
+                    cerr << number_pipe[cmd_count%1000].size() << endl;
+                    while(!number_pipe[cmd_count%1000].empty()){
+                        int fd = number_pipe[cmd_count%1000].back();
+                        dup2(fd,STDIN_FILENO);
+                        close(fd);
+                        number_pipe[cmd_count%1000].pop_back();
+                    }
+                }
+                if (i != s_input.size()-1 || is_number_pipe)
                     dup2(pipeline[pipe_count][1],STDOUT_FILENO);
                 if(s_input[i]==">"){
                     write_to_file((char*)(s_input[i+1].c_str()));    
                 }                                      
-
                 close_pipe(pipeline,pipe_count);
 
                 char* arg[tmp_input.size()+1];
@@ -80,17 +114,29 @@ void excute_cmd(vector<string> s_input){
             else{//paraent
                 if(s_input[i]==">")
                     i++;
-                int status;
-                pid_t waitpid;
-                //waitpid = wait(&status);
-                
                 if (pipe_count != 0){
                     close(pipeline[pipe_count-1][0]);
                     close(pipeline[pipe_count-1][1]);
                 } 
-                if (i == s_input.size()-1 && s_input[i][0] != '|'){
-                    close(pipeline[pipe_count][0]);
-                    close(pipeline[pipe_count][1]);
+                if (i == s_input.size()-1){
+                    if (!is_number_pipe){
+                        close(pipeline[pipe_count][0]);
+                        close(pipeline[pipe_count][1]);
+                    }
+                    else{
+                        //cerr << "is_number_pipe  " << cmd_count+number << " push " << pipeline[pipe_count][0] << endl;
+                        number_pipe[(cmd_count+number)%1000].push_back(pipeline[pipe_count][0]);
+                        close(pipeline[pipe_count][1]);
+                        //cerr << (cmd_count+number)%1000 <<"\t"<< number_pipe[(cmd_count+number)%1000].size()<<endl;
+                    }
+                }
+                if(have_number_pipe){
+                        cerr << "in_number_pipe" << endl;
+                    while(!number_pipe[cmd_count%1000].empty()){
+                        int fd = number_pipe[cmd_count%1000].back();
+                        close(fd);
+                        number_pipe[cmd_count%1000].pop_back();
+                    }
                 }
             }
             tmp_input.clear();
@@ -99,16 +145,21 @@ void excute_cmd(vector<string> s_input){
             tmp_input.push_back(s_input[i]);  
         } 
         i++;          
+        
+    }
+    for(int i=0;i<pidTable.size();i++){
+        int status;
+        waitpid(pidTable[i],&status,0);  
     }
 }
-
 
 int main(){
     string input;
     //setenv("PATH","./,bin/",1);
     setenv("PATH","bin",1);
     //cout << "PATH=" << getenv("PATH")<<endl;
-
+    vector<vector<int>> number_pipe(1000);
+    int cmd_count = 0;
     while(1){
         vector<string> s_input;        
         cout << "%";
@@ -117,6 +168,9 @@ int main(){
 
         if (s_input.size()<1)
             continue;
+        else
+            cmd_count += 1;
+        
         if (s_input[0]=="printenv"){
             cerr << getenv(s_input[1].c_str()) << endl;
             continue;
@@ -125,7 +179,6 @@ int main(){
             setenv(s_input[1].c_str(),s_input[2].c_str(),1);
             continue;
         }
-
-        excute_cmd(s_input);
+        excute_cmd(s_input,number_pipe,cmd_count);
     }
 }
